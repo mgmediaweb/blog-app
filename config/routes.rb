bb1a916bf55b5692da79b9b31539a965fecf6b3c
Gonzalo <mgmediaweb@gmail.com>
2022-09-13 11:54:58 -0700

    data structure created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  # get 'pages/home'
+  resources :comments
+  resources :posts
+  resources :users
+  # get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "pages#home"
+  root "home#index"
 end
