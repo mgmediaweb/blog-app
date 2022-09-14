88e528f52d165d26f4d2d17e1f2433974172db8d
Gonzalo <mgmediaweb@gmail.com>
2022-09-13 20:32:31 -0700

    make a comunitaion inter classes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_many :posts, foreign_key: 'user_id'
+  has_many :comments, foreign_key: 'user_id'
+  has_many :likes, foreign_key: 'user_id'
+
+  def last_three_posts
+    posts.where(user_id: self).order('created_at DESC').limit(3)
+  end
 end
