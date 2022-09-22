8f336a5e70a758ba0a091bd455bdb99409a68379
Gonzalo <mgmediaweb@gmail.com>
2022-09-22 15:54:56 -0700

    comments works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
   def index
     @title = 'List of users'
-    @users = User.all
+    @users = User.all.order(id: :asc)
   end
 
   def show
-    @user = User.find(params[:user_id])
+    @user = User.find(params[:id])
+    @comment = Comment.new
     @title = "Detail of #{@user.name}"
   end
 end
